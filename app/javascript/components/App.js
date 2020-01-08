19c68db2c93307eadba5a77e99e2b3440f741c5c
RahmanWijaya <r@2fast.ninja>
2020-01-08 11:58:46 +0700

    init commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+// import axios from 'axios'
+
+// --- Start Fix ---
+
+// --- End Fix ---
+
+class App extends Component {
+
+	render() {
+
+	  return (
+
+	    <div>MRTG DRPD Kota Batu</div>
+
+	  );
+
+	}
+
+}
+
+export default App;
