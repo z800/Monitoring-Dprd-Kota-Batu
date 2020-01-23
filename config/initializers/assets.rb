eb86123b4a9c58f854ed284855cf047cb56bbb1c
RahmanWijaya <r@2fast.ninja>
2020-01-23 13:30:08 +0700

    init commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,6 +13,17 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w(
   theme-night-admin.css
+  jquery/jquery-ui.min.css
+  fontawesome/font-awesome.min.css
+  codemirror/codemirror.css
+  nvd3/nv.d3.css
+  mcustomscrollbar/jquery.mCustomScrollbar.css
+  fullcalendar/fullcalendar.css
+  blueimp/blueimp-gallery.min.css
+  rickshaw/rickshaw.css
+  dropzone/dropzone.css
+  introjs/introjs.min.css
+  animate/animate.min.css
 )
 # cust.js
 # jQAllRangeSliders-min.js ion.rangeSlider.css
